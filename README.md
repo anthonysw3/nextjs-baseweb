d95782ca0c722c09bfa3c28e2f765054c0bd11d9
Anthony Weston <anthonyweston@Anthonys-MacBook-Pro.local>
2023-05-09 19:42:03 +0100

    Updated Readme

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a Nextjs and Base Web boilerplate.
 
 ## Getting Started
 
-First, run the development server:
+First, install the package:
+
+```bash
+npm install
+```
+
+Then, run the development server:
 
 ```bash
 npm run dev
@@ -16,9 +22,7 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
+## Nextjs
 
 To learn more about Next.js, take a look at the following resources:
 
@@ -27,8 +31,8 @@ To learn more about Next.js, take a look at the following resources:
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
-## Deploy on Vercel
+## Base Web
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+To learn more about Base Web, take a look at the following resources:
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- [Base Web Documentation](https://baseweb.design) - learn about Base Web features and API.
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,55 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import React from "react";
+
+import { Grid, Cell } from "baseui/layout-grid";
+import { ParagraphMedium, LabelMedium, LabelSmall } from "baseui/typography";
+import { Heading, HeadingLevel } from "baseui/heading";
+import { StyledDivider, SIZE } from "baseui/divider";
+import { StyledLink } from "baseui/link";
+import { StarRating } from "baseui/rating";
+import { Block } from "baseui/block";
 
 export default function Home() {
+  const [value, setValue] = React.useState(5);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <section>
+        <Grid>
+          <Cell span={[12]}>
+            <HeadingLevel>
+              <Heading styleLevel={4}>
+                Next.js 13.4.1 with Base Web 12.2.0
+              </Heading>
+              <LabelMedium>To get started, edit page.js</LabelMedium>
+              <Block paddingTop="20px" paddingBottom="20px">
+                <StarRating
+                  numItems={5}
+                  onChange={(data) => setValue(data.value)}
+                  size={22}
+                  value={value}
+                  readOnly
+                />
+              </Block>
+            </HeadingLevel>
+          </Cell>
+        </Grid>
+      </section>
+      <StyledDivider $size={SIZE.module} />
+      <section>
+        <Grid>
+          <Cell span={[12]}>
+            <Block paddingTop="10px">
+              <ParagraphMedium>
+                Check out{" "}
+                <StyledLink href="https://nextjs.org/">Next.js</StyledLink> and{" "}
+                <StyledLink href="https://baseweb.design">Base Web</StyledLink>.
+              </ParagraphMedium>
+            </Block>
+            <LabelSmall>Combined by Anthony Weston.</LabelSmall>
+          </Cell>
+        </Grid>
+      </section>
     </main>
-  )
+  );
 }
